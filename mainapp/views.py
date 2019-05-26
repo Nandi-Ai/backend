c10aedc286682d5d57141755a6a1e5f21da7d1d8
kobi <kobi@lynx.md>
2019-05-26 15:30:50 +0300

    fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -91,7 +91,7 @@ class GetExecution(APIView):
             if res.status_code != 201:
                 return Response({"error":"error creating execution"+str(res.text)})
 
-        return Response({'execution_identifier': execution.identifier, 'token': settings.jh_api_user_token}, status=201)
+        return Response({'execution_identifier': study.execution.identifier, 'token': settings.jh_api_user_token}, status=201)
 #
 # class DatasetManager(GenericAPIView):
 #     serializer_class = DatasetSerializer
