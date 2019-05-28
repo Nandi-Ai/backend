58860d8b99c5b8c09e0ade2c83979d9ceef3ab89
kobi <kobi@lynx.md>
2019-05-28 17:39:34 +0300

    with subrocess

--- mainapp/views.py
+++ mainapp/views.py
@@ -59,18 +59,21 @@ class GetExecutionConfig(APIView):
         config['bucket'] = study.name+"-"+study.organization.name+"-lynx-workspace"
         config['aws_sts_creds'] = response['Credentials']
 
-        import kubernetes.client
-        import kubernetes.config
-        from kubernetes.client.rest import ApiException
-        from pprint import pprint
-
-        kubernetes.config.load_kube_config()
-        api_instance = kubernetes.client.CoreV1Api()
-        try:
-            api_response = api_instance.connect_get_namespaced_pod_exec("jupyter-"+execution_identifier,namespace="jhub", command="python /usr/local/bin/sync_to_s3.py &")
-            pprint(api_response)
-        except ApiException as e:
-            print("Exception when calling CoreV1Api->connect_get_namespaced_pod_exec: %s\n" % e)
+        import subprocess
+        command = "kubectl exec jupyter-5417453f526c4bb98e84fceac84a141a -- python /usr/local/bin/sync_to_s3.py &"
+        subprocess.check_output(command.split(" "))
+        #
+        # import kubernetes.config
+        # from kubernetes.client.rest import ApiException
+        # from pprint import pprint
+        #
+        # kubernetes.config.load_kube_config()
+        # api_instance = kubernetes.client.CoreV1Api()
+        # try:
+        #     api_response = api_instance.connect_get_namespaced_pod_exec("jupyter-"+execution_identifier,namespace="jhub", command="python /usr/local/bin/sync_to_s3.py &")
+        #     pprint(api_response)
+        # except ApiException as e:
+        #     print("Exception when calling CoreV1Api->connect_get_namespaced_pod_exec: %s\n" % e)
 
         return Response(config)
 
