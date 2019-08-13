952dffe16330dde40424c87aaecd0c6895567936
kobi <kobi@lynx.md>
2019-08-13 14:50:35 +0300

    stop idle notebook servers

--- mainapp/lib.py
+++ mainapp/lib.py
@@ -10,7 +10,9 @@ from time import sleep
 import zipfile
 import subprocess
 import shutil
-
+import requests
+import json
+import pytz
 
 def break_s3_object(obj):
     file_name = obj.split("/")[-1]
@@ -19,6 +21,7 @@ def break_s3_object(obj):
     path = "/".join(obj.split("/")[:-1])
 
     return path, file_name, file_name_no_ext, ext
+
 
 def startup():
     os.environ["AWS_ACCESS_KEY_ID"] = settings.aws_access_key_id
@@ -156,6 +159,7 @@ def handle_zipped_data_source(data_source):
 # def clean(string):
 #     return ''.join(e for e in string.replace("-", " ").replace(" ", "c83b4ce5") if e.isalnum()).lower().replace("c83b4ce5", "-")
 
+
 def calc_access_to_database(user, dataset):
     if dataset.state == "private":
         if user.permission(dataset) == "aggregated":
@@ -171,3 +175,25 @@ def calc_access_to_database(user, dataset):
         return "full access"
 
     return "no permission"  # safe. includes archived dataset
+
+
+def close_all_jh_running_servers(idle_for_hours = 0):
+    import dateparser
+    from datetime import datetime as dt, timedelta as td, time as dttime
+    headers = {"Authorization": "Bearer " + settings.jh_api_admin_token}
+    res = requests.get(settings.jh_url+'/hub/api/users',headers=headers)
+    assert res.status_code == 200, "error getting users: "+res.text
+    users = json.loads(res.text)
+
+    for user in users:
+        if user['admin']:
+            continue
+        idle_time = dt.now(tz=pytz.UTC) - dateparser.parse(user['last_activity'])
+
+        if idle_time > td(hours=idle_for_hours):
+            res = requests.delete(settings.jh_url+'/hub/api/users/'+user['name']+'/server', headers=headers)
+            print("user", user['name'], "idle time:", idle_time, str(res.status_code), res.text)
+        else:
+            print(user['name'], "idle time:", idle_time, "<", td(hours=idle_for_hours))
+
+
