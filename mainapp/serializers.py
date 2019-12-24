ce427873f518fa6c7e5dfeb7eb96a5c82ac6f77e
annargmv <anna@lynx.md>
2019-12-24 11:59:27 +0200

    feat(documentation):Adding new documentaiton functionality

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -20,6 +20,14 @@ class OrganizationSerializer(ModelSerializer):
         model = Organization
         fields = ('id', 'name', 'logo',)
 
+class DocumentationSerializer(ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        many = kwargs.pop('many', True)
+        super(DocumentationSerializer, self).__init__(many=many, *args, **kwargs)
+
+    class Meta:
+        model = Documentation
+        fields = ('id', 'dataset', 'file_name')
 
 class TagSerializer(ModelSerializer):
     class Meta:
