adc5189bfae4fbcc0e0372659d1989564a74b7cc
annargmv <anna@lynx.md>
2020-06-22 12:03:38 +0300

    Update User's name in DB record

--- mainapp/serializers/user.py
+++ mainapp/serializers/user.py
@@ -7,3 +7,8 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "email", "organization", "name")
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "email": {"read_only": True},
+            "organization": {"read_only": True},
+        }
