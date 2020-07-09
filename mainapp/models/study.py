fbae96e1f9a550cd4f314b6a1f7684d66b5eb15f
Ofir Farchy <farchy@users.noreply.github.com>
2020-07-09 20:36:13 +0300

    added study status in DB

--- mainapp/models/study.py
+++ mainapp/models/study.py
@@ -13,6 +13,22 @@ logger = logging.getLogger(__name__)
 
 
 class Study(models.Model):
+    VM_CREATING = "creating"
+    VM_STARTING = "starting"
+    VM_ACTIVE = "active"
+    VM_STOPPING = "stopping"
+    VM_STOPPED = "stopped"
+    ST_ERROR = "error"
+    STUDY_DELETED = "deleted"
+    possible_statuses_for_study = (
+        (VM_CREATING, "creating"),
+        (VM_STARTING, "starting"),
+        (VM_ACTIVE, "active"),
+        (VM_STOPPING, "stopping"),
+        (VM_STOPPED, "stopped"),
+        (ST_ERROR, "error"),
+        (STUDY_DELETED, "deleted"),
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True, max_length=255)
@@ -33,6 +49,13 @@ class Study(models.Model):
     execution = models.ForeignKey(
         "Execution", on_delete=models.CASCADE, related_name="studies", null=True
     )
+    status = models.CharField(
+        choices=possible_statuses_for_study,
+        max_length=32,
+        null=False,
+        blank=False,
+        default="active",
+    )
     tags = models.ManyToManyField("Tag", related_name="study_tags")
     updated_at = models.DateTimeField(auto_now=True)
     cover = models.CharField(max_length=255, blank=True, null=True)
@@ -48,6 +71,8 @@ class Study(models.Model):
         logger.info(f"Deleting bucket {self.bucket} for study {self.name}:{self.id}")
         lib.delete_bucket(bucket_name=self.bucket, org_name=org_name)
         lib.delete_role_and_policy(bucket_name=self.bucket, org_name=org_name)
+
+    # TODO: add delete_instance(self, org_name)
 
     def __str__(self):
         return f"<Study id={self.id} name={self.name}>"
